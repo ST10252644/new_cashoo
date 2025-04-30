93a652faf05a01a7f6e5a9e7edaefc332cbf0480
ST10320489 <st10320489@vcconnect.edu.za>
2025-04-30 20:41:47 +0200

    Working code
    This file worked i promise

---
+++ app/src/main/java/com/iie/st10320489/marene/ui/rewards/Qr2Activity.kt
@@ -0,0 +1,35 @@
+package com.iie.st10320489.marene.ui.rewards
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.iie.st10320489.marene.R
+
+class Qr2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_qr2)
+
+
+        val btnContinue2: Button = findViewById(R.id.btnContinue2)
+
+        btnContinue2.setOnClickListener {
+            Toast.makeText(this, "THANK YOU !!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, FragmentRewardsTwo::class.java)
+            startActivity(intent)
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.qr2Main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
