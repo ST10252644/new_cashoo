a01d32d170feb47125cc1f335b2d6cb5aa26b48c
Cherika Bodde <st10252644@vcconnect.edu.za>
2025-05-01 18:54:54 +0200

    last fixes
    last fixes final working project

--- app/src/main/java/com/iie/st10320489/marene/ui/analysis/AnalysisViewModel.kt
+++
@@ -1,9 +0,0 @@
-
-
-package com.iie.st10320489.marene.ui.analysis
-
-import androidx.lifecycle.ViewModel
-
-class AnalysisViewModel : ViewModel() {
-    // You can add LiveData or other logic here in future
-}
--- app/src/main/java/com/iie/st10320489/marene/ui/rewards/ClaimItem.kt
+++
@@ -1,8 +0,0 @@
-package com.iie.st10320489.marene.ui.rewards
-
-data class ClaimItem(
-
-    val clmTitle: String,
-    val clmPoints: String,
-    val clmImageResId: Int
-)
